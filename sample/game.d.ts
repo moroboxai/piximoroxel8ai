bf6b4132bf294686f49717c53c2110e84275c904
Nauja <jeremymorosi@hotmail.fr>
2023-08-29 21:16:55 +0000

    Deploying to gh-pages from @ moroboxai/piximoroxel8ai@de2569f129539db8e3bce478fcc133df7791c29f 🚀

--- sample/game.d.ts
+++
@@ -1,4 +0,0 @@
-import { IPixiMoroxel8AI } from '../lib';
-export declare function init(vm: IPixiMoroxel8AI): void;
-export declare function load(): Promise<void>;
-export declare function tick(deltaTime: any): void;
