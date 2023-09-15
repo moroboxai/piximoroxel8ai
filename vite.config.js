7fcfc2d1c28d50973954ab371d5c84bfd69db6bd
Nauja <jeremymorosi@hotmail.fr>
2023-09-15 19:58:01 +0200

    improve the build

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,5 @@
 import { defineConfig } from "vite";
-const { resolve } = require("path");
 
 export default defineConfig({
-    build: {
-        outDir: resolve(__dirname, "lib"),
-        lib: {
-            entry: resolve(__dirname, "src", "index.ts"),
-            name: "PixiMoroxel8AI",
-            formats: ["cjs", "umd"],
-            fileName: (format) => {
-                switch (format) {
-                    case "cjs":
-                        return `cjs/index.cjs`;
-                    case "umd":
-                        return `umd/piximoroxel8ai.min.js`;
-                }
-            }
-        },
-        rollupOptions: {
-            input: {
-                main: "./index.html"
-            }
-        }
-    },
     base: "/piximoroxel8ai/"
 });
